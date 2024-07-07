b94f1d9697f9ccba67588742c1aa0cb21b5dad15
Nicola <horothesun@users.noreply.github.com>
2024-07-07 07:26:59 +0100

    More dependencies

--- src/main/g8/Main.scala
+++ src/main/g8/Main.scala
@@ -6,10 +6,14 @@
 //> using dep org.typelevel::cats-parse:1.0.0
 //> using dep org.typelevel::cats-effect:3.5.4
 //> using dep co.fs2::fs2-core:3.10.2
-//> using dep co.fs2::fs2-io:3.10.2
+//> using dep io.higherkindness::droste-core:0.9.0
 
+//> using test.dep org.typelevel::cats-laws:2.12.0
 //> using test.dep org.scalameta::munit:1.0.0
 //> using test.dep org.typelevel::munit-cats-effect:2.0.0
+//> using test.dep org.scalameta::munit-scalacheck:1.0.0
+//> using test.dep org.typelevel::cats-effect-testkit:3.5.4
+//> using test.dep org.typelevel::discipline-munit:2.0.0
 
 import cats.effect.*
 
