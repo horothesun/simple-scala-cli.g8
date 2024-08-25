70bba559aefa7f855f5b7b3dd797f87bc127f3e4
horothesun <horothesun@gmail.com>
2024-08-25 23:59:51 +0100

    Scala Native option

--- src/main/g8/Main.scala
+++ src/main/g8/Main.scala
@@ -3,6 +3,26 @@ $if(use_scala_3.truthy)$
 $else$
 //> using scala 2.13.14
 $endif$
+$if(use_scala_native.truthy)$
+//> using platform native
+//> using nativeVersion 0.4.17
+
+//> using dep org.typelevel::cats-core_native0.4:2.11.0
+//> using dep org.typelevel::kittens_native0.4:3.3.0
+//> using dep org.typelevel::cats-parse_native0.4:1.0.0
+//> using dep org.typelevel::cats-effect_native0.4:3.5.4
+//> using dep co.fs2::fs2-core_native0.4:3.10.2
+
+//> using test.dep org.scalameta::munit_native0.4:1.0.0
+//> using test.dep org.typelevel::munit-cats-effect_native0.4:2.0.0
+//> using test.dep org.scalameta::munit-scalacheck_native0.4:1.0.0-M11
+//> using test.dep org.scalacheck::scalacheck_native0.4:1.17.1
+//> using test.dep org.typelevel::scalacheck-effect-munit_native0.4:2.0.0-M2
+//> using test.dep org.typelevel::cats-effect-testkit_native0.4:3.5.4
+//> using test.dep org.typelevel::cats-laws_native0.4:2.11.0
+//> using test.dep org.typelevel::discipline-core_native0.4:1.6.0
+//> using test.dep org.typelevel::discipline-munit_native0.4:2.0.0-M3
+$else$
 //> using jvm temurin:21
 
 //> using dep org.typelevel::cats-core:2.12.0
@@ -21,6 +41,7 @@ $endif$
 //> using test.dep org.typelevel::cats-laws:2.12.0
 //> using test.dep org.typelevel::discipline-core:1.7.0
 //> using test.dep org.typelevel::discipline-munit:2.0.0
+$endif$
 
 $if(use_scala_3.truthy)$
 import cats.effect.*
