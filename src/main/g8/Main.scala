0c8d214a6deb087d0a0eb6eb82b181b662988146
Nicola <horothesun@users.noreply.github.com>
2024-11-09 18:37:35 +0100

    Update munit 1.0.2 and scalacheck 1.18.1

--- src/main/g8/Main.scala
+++ src/main/g8/Main.scala
@@ -29,10 +29,10 @@ $else$//> using jvm temurin:21
 //> using dep co.fs2::fs2-core:3.11.0
 //> using dep io.higherkindness::droste-core:0.9.0
 //
-//> using test.dep org.scalameta::munit:1.0.1
+//> using test.dep org.scalameta::munit:1.0.2
 //> using test.dep org.typelevel::munit-cats-effect:2.0.0
 //> using test.dep org.scalameta::munit-scalacheck:1.0.0
-//> using test.dep org.scalacheck::scalacheck:1.18.0
+//> using test.dep org.scalacheck::scalacheck:1.18.1
 //> using test.dep org.typelevel::scalacheck-effect-munit:1.0.4
 //> using test.dep org.typelevel::cats-effect-testkit:3.5.5
 //> using test.dep org.typelevel::cats-laws:2.12.0
