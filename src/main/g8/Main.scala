3937e7294339948b473657409a73e2013e0b2c88
Nicola <horothesun@users.noreply.github.com>
2024-11-26 23:55:56 +0100

    Update cats effect 3.5.7

--- src/main/g8/Main.scala
+++ src/main/g8/Main.scala
@@ -7,7 +7,7 @@ $if(use_scala_native.truthy)$//> using platform native
 //> using dep org.typelevel::cats-core_native0.4:2.11.0
 //> using dep org.typelevel::kittens_native0.4:3.3.0
 //> using dep org.typelevel::cats-parse_native0.4:1.0.0
-//> using dep org.typelevel::cats-effect_native0.4:3.5.6
+//> using dep org.typelevel::cats-effect_native0.4:3.5.7
 //> using dep co.fs2::fs2-core_native0.4:3.11.0
 //
 //> using test.dep org.scalameta::munit_native0.4:1.0.0
@@ -15,7 +15,7 @@ $if(use_scala_native.truthy)$//> using platform native
 //> using test.dep org.scalameta::munit-scalacheck_native0.4:1.0.0-M11
 //> using test.dep org.scalacheck::scalacheck_native0.4:1.17.1
 //> using test.dep org.typelevel::scalacheck-effect-munit_native0.4:2.0.0-M2
-//> using test.dep org.typelevel::cats-effect-testkit_native0.4:3.5.6
+//> using test.dep org.typelevel::cats-effect-testkit_native0.4:3.5.7
 //> using test.dep org.typelevel::cats-laws_native0.4:2.11.0
 //> using test.dep org.typelevel::discipline-core_native0.4:1.6.0
 //> using test.dep org.typelevel::discipline-munit_native0.4:2.0.0-M3
@@ -25,7 +25,7 @@ $else$//> using jvm temurin:21
 //> using dep org.typelevel::cats-core:2.12.0
 //> using dep org.typelevel::kittens:3.4.0
 //> using dep org.typelevel::cats-parse:1.0.0
-//> using dep org.typelevel::cats-effect:3.5.6
+//> using dep org.typelevel::cats-effect:3.5.7
 //> using dep co.fs2::fs2-core:3.11.0
 //> using dep io.higherkindness::droste-core:0.9.0
 //
@@ -34,7 +34,7 @@ $else$//> using jvm temurin:21
 //> using test.dep org.scalameta::munit-scalacheck:1.0.0
 //> using test.dep org.scalacheck::scalacheck:1.18.1
 //> using test.dep org.typelevel::scalacheck-effect-munit:1.0.4
-//> using test.dep org.typelevel::cats-effect-testkit:3.5.6
+//> using test.dep org.typelevel::cats-effect-testkit:3.5.7
 //> using test.dep org.typelevel::cats-laws:2.12.0
 //> using test.dep org.typelevel::discipline-core:1.7.0
 //> using test.dep org.typelevel::discipline-munit:2.0.0
